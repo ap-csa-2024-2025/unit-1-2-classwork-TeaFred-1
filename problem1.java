e82bb87f95f9dfcb4c5096bcedc8990790fea485
TeaFred <fhtruong100@student.hbuhsd.edu>
2024-09-04 11:05:50 -0700

    1/3 of the way done

--- problem1.java
+++ problem1.java
@@ -1,1 +1,8 @@
 // Create your class here; don't forget the main method
+public class problem1{
+    public static void main(String[] args)
+    {
+        String favoritefood = "chicken wings";
+        System.out.print("I like to eat " + favoritefood + " as well!");
+    }
+}
