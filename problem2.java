be1e5fc83069f5afece87535e6ee59832c8e53d0
TeaFred <fhtruong100@student.hbuhsd.edu>
2024-09-05 11:22:24 -0700

    Completed Problems 2 and 3

--- problem2.java
+++ problem2.java
@@ -1,1 +1,11 @@
 // Create your class here; don't forget the main method
+public class problem2{
+    public static void main(String[] args)
+    {
+        String name1 = "Alex";
+        String name2 = "Alexa";
+        String name3 = "Alexis";
+        String name4 = "Alec";
+        System.out.println(name4 + " " + name2 + " " + name3 + " " +name1);
+    }
+}
--- problem3.java
+++ problem3.java
@@ -1,1 +1,9 @@
 // Create your class here; don't forget the main method
+public class problem3{
+    public static void main(String[] args)
+    {
+        String MyName = "Fred";
+        String MyState = "California";
+        System.out.println("My name is " + MyName + ". " + "I live in " + MyState + ".");
+    }
+}
